18808722c9360195274b59fdc7c59d30ca99da45
Domitrii <126395589+Domitrii@users.noreply.github.com>
2023-11-03 11:34:33 +0100

    all

---
+++ js/task-1.js
@@ -0,0 +1,9 @@
+function slugify(title) {
+    let a = title.toLowerCase();
+    return a.split(" ").join("-")
+}
+
+console.log(slugify("Arrays for begginers")); 
+console.log(slugify("English for developer")); 
+console.log(slugify("Ten secrets of JavaScript")); 
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
